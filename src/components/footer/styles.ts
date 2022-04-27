4836f41ff4a18a263cf1a7432603836a15540d47
jamilly.pinheiro <jamilly.pinheiro@teclat.com.br>
2022-04-27 14:25:19 -0300

    Adicionando Footer

--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -1,5 +1,22 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    height: 165px;
+    left: 0px;
+    top: 847px;
+    background: #F5F5F5;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+`;
 
+export const Content = styled.div`
+    color: #321583;
+    text-align: center;
+
+    span, p {
+        font-weight: 400;
+        font-size: 14px;
+    }
 `;
