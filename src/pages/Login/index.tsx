a8e1c8d93d4be879a1de13cab7807044f8a26b64
jamilly.pinheiro <jamillyp@alu.ufc.br>
2022-06-10 14:42:33 -0300

    Corrigindo branch

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react';
 // import { Footer } from '../../components/Footer';
-import { Nav } from '../../components/Nav';
+import { Nav } from '../../components/Nav2';
 
 import { Container, LoginForm } from './styles';
 
